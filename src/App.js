dee2274fdf00a10d15bc91e42488079f755295f6
nawaz69 <54628281+nawaz69@users.noreply.github.com>
2022-12-27 03:09:05 +0500

    second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Do notLearn React
         </a>
       </header>
     </div>
